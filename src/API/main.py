b7948b1886050d93b84f2c48baa8e1e6263636d7
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-08-25 18:48:54 -0500

    NEW CALC

--- src/API/main.py
+++ src/API/main.py
@@ -5,7 +5,8 @@ import uvicorn
 
 
 from __init__ import * 
-from calc import *
+# from calc import *
+from alt_calc import *
 
 # -----------------------------------------------------------------------------
 app = FastAPI()
