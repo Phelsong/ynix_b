1bfcaf1cb73de53f6ae2f4253a575b2385b74a69
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-10-13 19:02:25 -0500

    clean up

--- src/DB/connection.py
+++ src/DB/connection.py
@@ -18,8 +18,8 @@ conn = (
         password=config["PASS_WORD"],
         row_factory=dict_row,
     )
-    if len(config) != 0
-    else psycopg.connect(conninfo=os.environ.get("DATABASE_URL"), row_factory=dict_row)
+    # if len(config) != 0
+    # else psycopg.connect(conninfo=os.environ.get("DATABASE_URL"), row_factory=dict_row)
 )
 # alt_conn = psycopg.connect(
 #     hostaddr=lab_db_server,
