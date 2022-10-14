30abd925b9dabac7a86f61da5e2feee7f4317a1c
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-10-13 20:54:37 -0500

    clean up

--- src/DB/connection.py
+++ src/DB/connection.py
@@ -9,18 +9,17 @@ config = dotenv_values(".env")
 
 
 # ----------------------------------------------------------------
-conn = (
-    psycopg.connect(
+conn = psycopg.connect(
         hostaddr=config["LAB_DB_SERVER"],
         port=config["LAB_DB_PORT"],
         dbname=config["LAB_DB"],
         user=config["USER_NAME"],
         password=config["PASS_WORD"],
-        row_factory=dict_row,
-    )
+        row_factory=dict_row)
+print(conn)
     # if len(config) != 0
     # else psycopg.connect(conninfo=os.environ.get("DATABASE_URL"), row_factory=dict_row)
-)
+
 # alt_conn = psycopg.connect(
 #     hostaddr=lab_db_server,
 #     port=lab_db_port,
