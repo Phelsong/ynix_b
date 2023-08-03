b847f3d56852bb2fcb1d337153d3fad72383f5b5
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2023-08-03 16:32:18 -0500

    scaffold

---
+++ data/pvp_mods.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# ----------------------------------------------------------------------------
+
+
+class ClassMod:
+    """Class vs Class PvP Modifiers"""
+
+    def __init__(self, name: str, *, classes):
+        self.name = name
+        class_mods.setdefault(self.name, self)
+
+
+# ============================================================================
+
+class_mods: dict[str, ClassMod] = {}
+
+
+warrior_mods = pd.read_csv("X:/0.code/ynix_b/data/PvP/warrior_vs.csv")
+
+
+for idx, row in warrior_mods.iterrows():
+    ClassMod(name="warrior")
